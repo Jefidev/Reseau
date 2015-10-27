c0afd39ddbc054ffa510e6ac94b55506822c676a
oceaneseel <oceane.seel@hotmail.com>
2015-10-28 00:09:10 +0100

    Bean CSV

--- JavaLibraryBeanBDAccess/src/DBAcess/BeanDBAccessCSV.java
+++ JavaLibraryBeanBDAccess/src/DBAcess/BeanDBAccessCSV.java
@@ -2,11 +2,10 @@ package DBAcess;
 
 import java.io.Serializable;
 import java.sql.*;
-import java.util.ArrayList;
 import java.util.HashMap;
 
 
-public class BeanDBAccessCSV implements Serializable, InterfaceBeansDBAccess
+public class BeanDBAccessCSV implements Serializable
 {    
     private String ip;
     private int port;
@@ -18,71 +17,14 @@ public class BeanDBAccessCSV implements Serializable, InterfaceBeansDBAccess
     
     public BeanDBAccessCSV() {
     }
+
     
-    
-    /* GETTERS ET SETTERS */
-    
-    @Override
-    public String getIp() {
-        return ip;
-    }
-    @Override
-    public void setIp(String value) {
-        ip = value;
-    }
-    
-    @Override
-    public int getPort() {
-        return port;
-    }
-    @Override
-    public void setPort(int value) {
-        port = value;
-    }
-    
-    @Override
-    public String getBd() {
-        return bd;
-    }
-    @Override
-    public void setBd(String value) {
-        bd = value;
-    }
-    
-    @Override
-    public String getUser() {
-        return user;
-    }
-    @Override
-    public void setUser(String value) {
-        user = value;
-    }
-    
-    @Override
-    public String getPassword() {
-        return pwd;
-    }
-    @Override
-    public void setPassword(String value) {
-        pwd = value;
-    }
-    
-    @Override
-    public void setClient(InterfaceRequestListener c) {
-        client = c;
-    }
-    
-    
-    /* BASE DE DONNEES */
-    
-    @Override
     public void connexion()
     {
         try
         {
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-            String url = "jdbc:oracle:thin:" + getUser() + "/" + getPassword() + "@" + getIp() + ":" + getPort() + ":" + getBd();
-            con = DriverManager.getConnection(url);
+            Class.forName("jstels.jdbc.csv.CsvDriver2");
+            con = DriverManager.getConnection("jdbc:jstels:csv:.?separator=;");
             con.setAutoCommit(false);
         }
         catch (SQLException ex)
@@ -94,34 +36,7 @@ public class BeanDBAccessCSV implements Serializable, InterfaceBeansDBAccess
             System.out.println("Driver adéquat non trouvable : " + ex.getMessage());
         }
     }
-    
-    @Override
-    public ArrayList<String> tablesDisponibles()
-    {
-        ArrayList<String> l = new ArrayList<String>();
-        
-        try
-        {
-            DatabaseMetaData md = con.getMetaData();
-            ResultSet tables = md.getTables(con.getCatalog(), getUser(), null, null);
 
-            while (tables.next())
-            {
-                String t = tables.getString(3);
-                l.add(t);
-            }
-
-            return l;
-        }
-        catch (SQLException ex)
-        {
-            System.out.println("Erreur SQL : " + ex.getMessage());
-        }
-        
-        return null;
-    }
-    
-    @Override
     public Thread selection(String s, String f, String w)
     {
         ReadingThreadDBAccess rt = new ReadingThreadDBAccess(con, s, f, w, client);
@@ -129,8 +44,7 @@ public class BeanDBAccessCSV implements Serializable, InterfaceBeansDBAccess
         
         return rt;
     }
-    
-    @Override
+
     public Thread ecriture(String f, HashMap d)
     {
         WritingThreadDBAccess wt = new WritingThreadDBAccess(con, f, d, client);
@@ -139,7 +53,6 @@ public class BeanDBAccessCSV implements Serializable, InterfaceBeansDBAccess
         return wt;
     }
     
-    @Override
     public Thread miseAJour(String f, HashMap d, String w)
     {
         WritingThreadDBAccess wt = new WritingThreadDBAccess(con, f, d, w, client);
@@ -148,7 +61,6 @@ public class BeanDBAccessCSV implements Serializable, InterfaceBeansDBAccess
         return wt;
     }
     
-    @Override
     public void finConnexion()
     {
         try
@@ -159,5 +71,9 @@ public class BeanDBAccessCSV implements Serializable, InterfaceBeansDBAccess
         {
             System.out.println("SQLException : " + ex);
         }
+    }
+    
+    public void setClient(InterfaceRequestListener c) {
+        client = c;
     }
 }
