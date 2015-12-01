48795c5a2c083a8ab8874e15df55f1e3aeaa47e1
Jérôme Fink <jeromefink@hotmail.com>
2015-12-01 20:08:34 +0100

    class maïs

---
+++ Applic_Data_Mining/src/applic_data_mining/Mais.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package applic_data_mining;
+
+/**
+ *
+ * @author Jerome
+ */
+public class Mais {
+    
+    private int Hauteur;
+    private int Masse;
+    private int NbrGrains;
+    private Double MasseGrain;
+    private String Couleur;
+    private boolean Germination;
+    private String Enracinement;
+    private boolean Verse; //Verse : maladie qui touche le Maïs entre autre (baisse de rendement)
+    private boolean Attaque;
+    private String Parcelle;
+    
+    
+}
