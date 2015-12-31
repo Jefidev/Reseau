4d47fac6ef455c5046135dc464a003efbed0c443
oceaneseel <oceane.seel@hotmail.com>
2015-12-31 17:45:23 +0100

    Début script d'insertion db compta

--- Serveur_Compta/src/serveur_compta/Runnable_BISAMAP.java
+++ Serveur_Compta/src/serveur_compta/Runnable_BISAMAP.java
@@ -9,8 +9,6 @@ import java.net.Socket;
 import java.security.MessageDigest;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import newBean.BeanBDAccess;
 import newBean.connexionException;
 
