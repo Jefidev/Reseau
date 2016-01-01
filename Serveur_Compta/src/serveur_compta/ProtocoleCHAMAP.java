e543cc8e47a79dd2dd7d20ccd4923d3893b78390
oceaneseel <oceane.seel@hotmail.com>
2016-01-01 22:03:36 +0100

    Protocoles CHAMAP

---
+++ Serveur_Compta/src/serveur_compta/ProtocoleCHAMAP.java
@@ -0,0 +1,9 @@
+package serveur_compta;
+
+
+public class ProtocoleCHAMAP
+{   
+    public static final int LOGIN_TRAF = 1;
+    public static final int MAKE_BILL = 2;
+    public static final int LOGOUT = 3;
+}                    
