45b7d9f7ba3d366d03b43b95c41c6972737a860d
oceaneseel <oceane.seel@hotmail.com>
2016-01-19 19:05:47 +0100

    GetNextBill app avant souper

--- Application_Compta/src/application_compta/ValidationFacture.java
+++ Application_Compta/src/application_compta/ValidationFacture.java
@@ -1,6 +1,7 @@
 package application_compta;
 
 import javax.swing.SwingUtilities;
+import library_compta.ProtocoleBISAMAP;
 
 
 public class ValidationFacture extends javax.swing.JPanel
@@ -16,6 +17,9 @@ public class ValidationFacture extends javax.swing.JPanel
     private void initComponents() {
 
         MenuButton = new javax.swing.JButton();
+        TitreLabel = new javax.swing.JLabel();
+        NextBill = new javax.swing.JButton();
+        ValiderFacture = new javax.swing.JButton();
 
         MenuButton.setLabel("Retour au menu");
         MenuButton.addActionListener(new java.awt.event.ActionListener() {
@@ -24,20 +28,53 @@ public class ValidationFacture extends javax.swing.JPanel
             }
         });
 
+        TitreLabel.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        TitreLabel.setForeground(new java.awt.Color(0, 0, 255));
+        TitreLabel.setText("FACTURE PLUS ANCIENNE NON VALIDEE :");
+
+        NextBill.setText("Prochaine facture");
+        NextBill.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                NextBillActionPerformed(evt);
+            }
+        });
+
+        ValiderFacture.setText("Valider la facture");
+        ValiderFacture.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ValiderFactureActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(431, Short.MAX_VALUE)
-                .addComponent(MenuButton)
-                .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(NextBill)
+                        .addGap(18, 18, 18)
+                        .addComponent(ValiderFacture)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(MenuButton)
+                        .addContainerGap())
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(0, 78, Short.MAX_VALUE)
+                        .addComponent(TitreLabel)
+                        .addGap(84, 84, 84))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(266, Short.MAX_VALUE)
-                .addComponent(MenuButton)
+                .addContainerGap()
+                .addComponent(TitreLabel)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 233, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(MenuButton)
+                    .addComponent(NextBill)
+                    .addComponent(ValiderFacture))
                 .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
@@ -48,8 +85,31 @@ public class ValidationFacture extends javax.swing.JPanel
         app.ChangePanel("Menu");
     }//GEN-LAST:event_MenuButtonActionPerformed
 
+    private void NextBillActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_NextBillActionPerformed
+        Utility.SendMsg(ProtocoleBISAMAP.GET_NEXT_BILL, "");
+        
+        String reponse = Utility.ReceiveMsg();  
+        String[] parts = reponse.split("#");
+        
+        if (parts[0].equals("OUI"))
+        {
+        
+        }
+        else
+        {
+        
+        }
+    }//GEN-LAST:event_NextBillActionPerformed
+
+    private void ValiderFactureActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ValiderFactureActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_ValiderFactureActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton MenuButton;
+    private javax.swing.JButton NextBill;
+    private javax.swing.JLabel TitreLabel;
+    private javax.swing.JButton ValiderFacture;
     // End of variables declaration//GEN-END:variables
 }
